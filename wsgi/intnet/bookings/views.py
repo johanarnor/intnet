d96398318e819f0fe88f9315c6f2490e3cf39b0e
Jonas <jonas.stendahl@outlook.com>
2014-03-06 09:59:20 +0100

    Signup working

--- wsgi/intnet/bookings/views.py
+++ wsgi/intnet/bookings/views.py
@@ -3,7 +3,7 @@ from bookings.models import Booking
 
 
 def booking(request, booking_id):
-    current_booking = Booking.objects.get(pk=booking_id);
+    current_booking = Booking.objects.get(pk=booking_id)
     people = current_booking.people_set.all()
     p = people[0]
     total = p.senior + p.adult + p.youth + p.child + p.student
