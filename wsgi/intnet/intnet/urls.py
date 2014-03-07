39a6872bb35bd96a2232322e63a10894c0f2b27e
Johan <jarnor@kth.se>
2014-03-07 10:51:20 +0100

    Carousel fungerar nu. Plockar ut de tre populäraste ur databasen och lägger in dess bild i den (förutsatt att den existerar) Har dumpat ny data.

--- wsgi/intnet/intnet/urls.py
+++ wsgi/intnet/intnet/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,4 +11,4 @@ urlpatterns = patterns('',
                        url(r'^', include('main.urls', namespace='main')),
                        url(r'^activities/', include('activities.urls', namespace='activities')),
                        url(r'^bookings/', include('bookings.urls', namespace='bookings')),
-                       )
+                       ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- wsgi/intnet/main/views.py
+++ wsgi/intnet/main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from activities.models import Activity
-
-# Create your views here.
+from activities.models import Activity, ActivityImage
 
 
 def main(request):
+    carousel_activities = Activity.objects.order_by('-purchases')[:3]
+    carousel_images = ActivityImage.objects.filter(id__in=carousel_activities)
     activities = Activity.objects.all()
-    return render(request, 'main/main.html', {'activities': activities})
+    return render(request, 'main/main.html', {'activities': activities, 'carousel_images': carousel_images})
