6940008a3cf7159bfb4e7bcba7320fbd29daea91
Johan <jarnor@kth.se>
2014-03-07 17:15:10 +0100

    Thumbnails klara på main-sidan. Ska lägga till boknings-knappar.

--- wsgi/intnet/main/views.py
+++ wsgi/intnet/main/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
-from activities.models import Activity, CarouselImage
+from activities.models import Activity, CarouselImage, DescriptionImage
 
 
 def main(request):
+    activities = Activity.objects.all()
+
     carousel_activities = Activity.objects.order_by('-purchases')[:3]
     carousel_images = CarouselImage.objects.filter(id__in=carousel_activities)
-    activities = Activity.objects.all()
-    return render(request, 'main/main.html', {'activities': activities, 'carousel_images': carousel_images})
+
+    description_images = []
+    for activity in activities:
+        description_images.append(activity.descriptionimage_set.all()[0].image.url)
+
+    print description_images
+    activities_images = zip(activities, description_images)
+
+    return render(request, 'main/main.html', {'activities': activities, 'carousel_images': carousel_images,
+                                              'activities_images': activities_images})
