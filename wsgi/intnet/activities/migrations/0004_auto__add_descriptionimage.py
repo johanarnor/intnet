fc0076d3d6cded05acd1cd5632ffb99ed1f7fef7
Johan <jarnor@kth.se>
2014-03-07 15:41:10 +0100

    Forgot the migration file.

---
+++ wsgi/intnet/activities/migrations/0004_auto__add_descriptionimage.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'DescriptionImage'
+        db.create_table(u'activities_descriptionimage', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('image', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+            ('activity', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['activities.Activity'])),
+        ))
+        db.send_create_signal(u'activities', ['DescriptionImage'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'DescriptionImage'
+        db.delete_table(u'activities_descriptionimage')
+
+
+    models = {
+        u'activities.activity': {
+            'Meta': {'object_name': 'Activity'},
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'price': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['activities.Price']"}),
+            'purchases': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        u'activities.carouselimage': {
+            'Meta': {'object_name': 'CarouselImage'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['activities.Activity']"}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'activities.descriptionimage': {
+            'Meta': {'object_name': 'DescriptionImage'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['activities.Activity']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.FileField', [], {'max_length': '100'})
+        },
+        u'activities.feature': {
+            'Meta': {'object_name': 'Feature'},
+            'activity': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['activities.Activity']"}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'feature': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'activities.featureoption': {
+            'Meta': {'object_name': 'FeatureOption'},
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'feature': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['activities.Feature']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'option': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        u'activities.price': {
+            'Meta': {'object_name': 'Price'},
+            'adult': ('django.db.models.fields.IntegerField', [], {}),
+            'child': ('django.db.models.fields.IntegerField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'senior': ('django.db.models.fields.IntegerField', [], {}),
+            'student': ('django.db.models.fields.IntegerField', [], {}),
+            'youth': ('django.db.models.fields.IntegerField', [], {})
+        }
+    }
+
+    complete_apps = ['activities']
