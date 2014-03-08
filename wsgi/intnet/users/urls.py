cc90c91ccea2ccf756754c409de1b16441a43401
Johan <jarnor@kth.se>
2014-03-08 15:44:13 +0100

    Lagt till en profil-sida.

--- wsgi/intnet/users/urls.py
+++ wsgi/intnet/users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^auth/$', views.authenticate_view, name='authenticate'),
     url(r'^logout/$', views.logout_view, name='logout'),
     url(r'^sign_up/$', views.sign_up_view, name='sign_up'),
+    url(r'^profile/$', views.profile_view, name='profile'),
+    url(r'^save_user/$', views.save_user_view, name='save_user'),
     )
